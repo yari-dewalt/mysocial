de386d6a18e630c8b223485875da0badb85397da
yari-dewalt <yari.dewalt@gmail.com>
2024-05-09 21:29:37 -0700

    Remove .js files

--- src/socket.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.socket = void 0;
-var socket_io_client_1 = require("socket.io-client");
-var URL = "https://mysocial-backend.onrender.com";
-exports.socket = (0, socket_io_client_1.io)(URL);
